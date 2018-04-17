17d1d96fa2ceb320878ff9e3ba758ed7b092d87b
Meher Khan <33574476+meher007@users.noreply.github.com>
2018-04-17 23:32:35 +0100

    Add files via upload
    Take some random integers from user and make them in ascending order!

---
+++ AscendingOrder.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatest;
+
+import java.util.Scanner;
+/**
+ *Take number of element(integer) from user, then allow user to enter element (integer) one by one
+ * Compare the integer into next integer if next integer is less then the first integer then swipe the value 
+ * by using a extra memory 
+ * This way make ascending order of all the element however, it does not remove duplicate integer
+ * 
+ * @author Meher Khan
+ */
+
+public class AscendingOrder
+{
+    public static void main(String[] args) 
+    {
+        int n, temp;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter no. of elements you want in array:"); //Takes user request number of elements
+        n = s.nextInt();
+        int a[] = new int[n];
+        System.out.println("Enter all the elements:");
+        for (int i = 0; i < n; i++) // for loop is using to store all the integer one by one
+        {
+            a[i] = s.nextInt();
+        }
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = i + 1; j < n; j++) 
+            {
+                if (a[i] > a[j]) // if first integer value is greater compare to second intger    
+                {
+                    temp = a[i]; // then store first integer value in a new memory and
+                    a[i] = a[j];// replace first integer value by second integer
+                    a[j] = temp;// replace second integer value by second integer (new memory)
+                }
+            }
+        }
+        System.out.print("Ascending Order: ");
+        for (int i = 0; i < n - 1; i++) 
+        {
+            System.out.print(a[i] + ",");
+        }
+        System.out.print(a[n - 1]+" ");
+    }
+}
