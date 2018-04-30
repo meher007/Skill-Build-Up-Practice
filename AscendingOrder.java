b4b269be4f255f5c9228d07b68d0cebe24f51ff1
Meher Khan <33574476+meher007@users.noreply.github.com>
2018-04-30 21:38:40 +0100

    Sorting integer array into ascending order

--- AscendingOrder.java
+++ AscendingOrder.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javatest;
 
 import java.util.Scanner;
@@ -17,7 +12,7 @@ import java.util.Scanner;
 
 public class AscendingOrder
 {
-    public static void main(String[] args) 
+    public static void main(String... args) //Another way declar main method
     {
         int n, temp;
         Scanner s = new Scanner(System.in);
